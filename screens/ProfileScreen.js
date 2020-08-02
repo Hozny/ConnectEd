74d6212156b215e087192944d95d6a0e8ff4d7c7
ChealsieBains01 <4chealsie.9@gmail.com>
2020-08-02 00:39:57 -0400

    Connected profile to firestore

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,16 +1,46 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { firestore, storage } from "firebase";
+import firebase from "firebase";
 
-export default function ProfileScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class ProfileScreen extends React.Component {
+  state = {
+    user: {
+      name: "",
+      major: "",
+      graduatingYear: "",
+    },
+  };
+  constructor(props) {
+    super(props);
+    this.userId = firebase.auth().currentUser.uid;
+    this.profile = firestore()
+      .collection("users")
+      .doc(this.userId)
+      .onSnapshot((doc) => {
+        this.setState({
+          user: {
+            name: doc.data().name,
+            major: doc.data().major,
+            graduatingYear: doc.data().graduatingYear,
+          },
+        });
+      });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.user.name}</Text>
+        <Text>{this.state.user.major}</Text>
+        <Text>Class of {this.state.user.graduatingYear}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
